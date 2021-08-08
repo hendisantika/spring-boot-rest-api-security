961826d0456df28fb471a8bb067cb4221c76b706
Hendi Santika <hendisantika@gmail.com>
2021-08-08 10:12:44 +0700

    #20 - Add passwordEncoder bean in SecurityConfig

--- src/main/java/com/hendisantika/springbootrestapisecurity/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/springbootrestapisecurity/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.hendisantika.springbootrestapisecurity.config;
 
 import com.hendisantika.springbootrestapisecurity.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AuthenticationService authenticationService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
