9dbb6c42a93bba4f3f816300009a785ed6a4db16
Hendi Santika <hendisantika@gmail.com>
2021-08-08 10:09:24 +0700

    #17 - Add BeanNotFound in AuthenticationService

---
+++ src/main/java/com/hendisantika/springbootrestapisecurity/exception/BeanNotFound.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootrestapisecurity.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/08/21
+ * Time: 10.08
+ */
+public class BeanNotFound extends Exception {
+
+    public BeanNotFound(String message) {
+        super(message);
+    }
+}
