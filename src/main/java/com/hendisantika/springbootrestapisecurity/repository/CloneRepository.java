043f03cffd49f0aa7f60ca8930ee74a20928865d
Hendi Santika <hendisantika@gmail.com>
2021-08-08 10:01:02 +0700

    #9 - Add CloneRepository

---
+++ src/main/java/com/hendisantika/springbootrestapisecurity/repository/CloneRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootrestapisecurity.repository;
+
+import com.hendisantika.springbootrestapisecurity.entity.Clone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/08/21
+ * Time: 10.00
+ */
+public interface CloneRepository extends JpaRepository<Clone, Long> {
+}
