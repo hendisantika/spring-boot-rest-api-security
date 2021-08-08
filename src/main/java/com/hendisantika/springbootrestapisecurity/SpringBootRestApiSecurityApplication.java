923b4004c38d68a0d3c3628c636239ccab191b13
Hendi Santika <hendisantika@gmail.com>
2021-08-08 09:40:48 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootrestapisecurity/SpringBootRestApiSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootrestapisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiSecurityApplication.class, args);
+    }
+
+}
