5e3201efe9bee9fb9dd3dd40b7a0b35ea8607f97
Hendi Santika <hendisantika@gmail.com>
2021-08-08 10:13:11 +0700

    #21 - Add configure in SecurityConfig

--- src/main/java/com/hendisantika/springbootrestapisecurity/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/springbootrestapisecurity/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.hendisantika.springbootrestapisecurity.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +32,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers("/kamino/**").hasAnyRole("KAMINOAIN", "EMPEROR")
+                .and()
+                .authorizeRequests().antMatchers("/console/**").permitAll()// pour continuer à avoir accès à la
+                // console de la bdd
+                .and()
+                .httpBasic();
+        http.csrf().disable().cors().disable();
+        http.headers().frameOptions().disable();
+
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .eraseCredentials(true)
+                .userDetailsService(authenticationService)
+                .passwordEncoder(passwordEncoder());
     }
 
 }
