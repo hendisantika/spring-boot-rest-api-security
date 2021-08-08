a692f15fcb129c33f5ed1677577439b2f888a67c
Hendi Santika <hendisantika@gmail.com>
2021-08-08 09:58:47 +0700

    #7 - Add AccountRole Enum

---
+++ src/main/java/com/hendisantika/springbootrestapisecurity/entity/AccountRole.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootrestapisecurity.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/08/21
+ * Time: 09.58
+ */
+public enum AccountRole {
+    KAMINOAIN, EMPEROR
+}
