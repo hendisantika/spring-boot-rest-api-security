0ae6d6101e021d5225111b3f210ec30f85e35c0c
Hendi Santika <hendisantika@gmail.com>
2021-08-08 10:18:45 +0700

    #24 - Add findAllPages in HumanCloningController

--- src/main/java/com/hendisantika/springbootrestapisecurity/controller/HumanCloningController.java
+++ src/main/java/com/hendisantika/springbootrestapisecurity/controller/HumanCloningController.java
@@ -3,6 +3,11 @@ package com.hendisantika.springbootrestapisecurity.controller;
 import com.hendisantika.springbootrestapisecurity.entity.Clone;
 import com.hendisantika.springbootrestapisecurity.repository.CloneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +35,14 @@ public class HumanCloningController {
     @PreAuthorize("hasAnyAuthority('ROLE_KAMINOAIN', 'ROLE_EMPEROR')")
     public List<Clone> findAll() {
         return cloneRepository.findAll();
+    }
+
+    @GetMapping("/pages")
+    @PreAuthorize("hasAnyAuthority('ROLE_KAMINOAIN', 'ROLE_EMPEROR')")
+    public Page<Clone> findAllPages(@PageableDefault(page = 0, size = 20)
+                                    @SortDefault.SortDefaults({
+                                            @SortDefault(sort = "id", direction = Sort.Direction.ASC)
+                                    }) Pageable pageable) {
+        return cloneRepository.findAll(pageable);
     }
 }
