cc9e7cbe6b0a19e3af3d7df22cb00870488d053d
Hendi Santika <hendisantika@gmail.com>
2021-08-08 10:03:21 +0700

    #11 - Add initialize in AccountService

--- src/main/java/com/hendisantika/springbootrestapisecurity/service/AccountService.java
+++ src/main/java/com/hendisantika/springbootrestapisecurity/service/AccountService.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springbootrestapisecurity.service;
 
+import com.hendisantika.springbootrestapisecurity.entity.Account;
+import com.hendisantika.springbootrestapisecurity.entity.AccountRole;
 import com.hendisantika.springbootrestapisecurity.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +26,15 @@ public class AccountService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void initialize() {
+        if (accountRepository.findOneByUsername("kamino") == null) {
+            save(new Account("kamino", "kamino", AccountRole.KAMINOAIN.name()));
+        }
+        if (accountRepository.findOneByUsername("palpatine") == null) {
+            save(new Account("palpatine", "palpatine", AccountRole.EMPEROR.name()));
+        }
+    }
 
 }
